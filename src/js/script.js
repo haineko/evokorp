cb894c598b89bebb24bff4a6f548e819b3d618d2
Ann Fadeeva <haineko1512@mail.ru>
2016-05-20 17:07:15 +0300

    fix filter

--- src/js/script.js
+++ src/js/script.js
@@ -16,12 +16,26 @@ function ready () {
   if (isChrome && isMac) {
     className = 'mac-chrome';
   }
-  if (!isMac && isChrome) {
-    className = 'win-chrome';
-  }
 
   body.addClass(className);
   console.log("закончился");
+
+  $('.partner__item').BlackAndWhite({
+      hoverEffect : true, // default true
+      // set the path to BnWWorker.js for a superfast implementation
+      webworkerPath : false,
+      // to invert the hover effect
+      invertHoverEffect: false,
+      // this option works only on the modern browsers ( on IE lower than 9 it remains always 1)
+      intensity:1,
+      speed: { //this property could also be just speed: value for both fadeIn and fadeOut
+          fadeIn: 200, // 200ms for fadeIn animations
+          fadeOut: 800 // 800ms for fadeOut animations
+      },
+      onImageReady:function(img) {
+          // this callback gets executed anytime an image is converted
+      }
+  });
 }
 
  document.addEventListener("DOMContentLoaded", ready);
